3756779645e7ef67483d3ea8878945c98dbc209e
couderbe <couderc.benoit@orange.fr>
2022-05-10 10:47:32 +0200

    Analyser connected to investigations

--- analyser/views.py
+++ analyser/views.py
@@ -7,27 +7,21 @@ from analyser.elements.dump import Dump
 from analyser.elements.node import Node
 from analyser.elements.process import Process
 from analyser.elements.analysis import Analysis
+from investigations.forms import ManageInvestigation
 
 from investigations.models import UploadInvestigation
 
 
 @login_required
 def analyser(request):
-    with open('Cases/Results/'+'12'+'.json') as f:
-        investData = json.load(f)
-    # caseData = UploadInvestigation.objects.get(pk=1)
-    # print(caseData)
-    # print(type(caseData))
-    dmp = Dump("Example Memory Dump")
-    processes = [Process(name=elt['ImageFileName'],
-                         pid=elt['PID'], ppid=elt['PPID'], sessionId=elt['SessionId'], wow64=elt['Wow64'], createTime=elt['CreateTime'], exitTime=elt['ExitTime']) for elt in investData['psscan']]
-    dmp.children.extend(processes)
-    analysis = Analysis("Analyse")
-    analysis.loadDump(investData)
-    print(analysis.toChart())
-    dataJSON = dumps(analysis.toDict())
-    #dataJSON = dumps(analysis.toChart())
-    #dataJSON = dumps(context)
-    groups = dumps([sub.__name__ for sub in Node.__subclasses__()])
-    print(groups)
-    return render(request, 'analyser/analyser.html', {'data': dataJSON,'groups':groups})#,'dataTree':dumps(analysis.toDict())})
+    if request.method == 'GET':
+        form = ManageInvestigation(request.GET)
+        if form.is_valid():
+            id = form.cleaned_data['sa_case_id']
+            with open('Cases/Results/'+str(id)+'.json') as f:
+                investData = json.load(f)
+            analysis = Analysis("Analyse")
+            analysis.loadDump(investData)
+            dataJSON = dumps(analysis.toDict())
+            groups = dumps([sub.__name__ for sub in Node.__subclasses__()])
+            return render(request, 'analyser/analyser.html', {'data': dataJSON,'groups':groups})
